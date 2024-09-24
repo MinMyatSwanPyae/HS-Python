b7c35f37860f49ee6bb3ae4385827b6e01058dfb
MinMyatSwanPyae <73636287+MinMyatSwanPyae@users.noreply.github.com>
2024-09-24 17:09:08 +0700

    Sep24

---
+++ String.py
@@ -0,0 +1,54 @@
+# String 
+
+empty_string = "" #declaring empty string 
+empty_string = '' #declaring empty string 
+empty_string = """ """ #declaring multi line empty string 
+empty_string = str()
+
+s  = "Jedi Loves Ve"
+s = """ 
+Jedi loves Ve 
+Jedi loves Aomsub 
+Jedi loves Soda
+Jedi loves Pizza 
+"""
+
+my_sent = " I'm a boy"
+my_sent = 'she said that \"I am a boy\"'
+
+s1 = "Hello, "
+s2 = "Vetiti"
+
+s3 = "Palm"
+
+# print(s3 * 3)
+
+len(s)
+
+# String 
+
+empty_string = "" #declaring empty string 
+empty_string = '' #declaring empty string 
+empty_string = """ """ #declaring multi line empty string 
+empty_string = str()
+
+s  = "Jedi Loves Ve"
+s = """ 
+Jedi loves Ve 
+Jedi loves Aomsub 
+Jedi loves Soda
+Jedi loves Pizza 
+"""
+
+my_sent = " I'm a boy"
+my_sent = 'she said that \"I am a boy\"'
+
+s1 = "Hello, "
+s2 = "Vetiti"
+
+s3 = "Palm"
+
+# print(s3 * 3)
+
+len(s)
+
---
+++ debugger_trial.py
@@ -0,0 +1,5 @@
+i = 2 
+while i < 5: 
+    for j in range (1, 5, 2):
+        print (i, j)
+        i +=1
